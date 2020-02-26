e4794c469fa323f2aacd296edabc3941f9c3e6eb
Eva Xu <eva@quantstamp.com>
2020-02-26 14:58:43 -0500

    fix style

--- truffle.js
+++ truffle.js
@@ -7,16 +7,15 @@ module.exports = {
         enabled: true,
         runs: 200,
       },
-    evmVersion: "istanbul",
+      evmVersion: "istanbul",
     }
   },
   networks: {
-      development: {
-        host: "127.0.0.1",
-        port: 8545,
-        network_id: "*", // Match any network id
-        gas: 6712388
-      }
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*", // Match any network id
+      gas: 6712388
+    }
   }
 };
-  

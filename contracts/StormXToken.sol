7bde74a4b41f36bef3f9e159ec05c06c98fb2603
Eva Xu <eva@quantstamp.com>
2020-02-27 09:42:09 -0500

    extend erc20Detailed

--- contracts/StormXToken.sol
+++ contracts/StormXToken.sol
@@ -1,20 +1,13 @@
 pragma solidity 0.5.16;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
-contract StormXToken is ERC20 {
-
-  // Public variables of the token
+contract StormXToken is ERC20Detailed("Storm Token", "STORM", 18), ERC20 {
   string public standard;
-  string public name;
-  string public symbol;
-  uint8 public decimals;
 
   constructor() public {
     standard = "Storm Token v2.0";
-    name = "Storm Token";
-    symbol = "STORM"; // token symbol
-    decimals = 18;
   }
 }
